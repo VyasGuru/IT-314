dfbf884b7aaa9918216ed65ae0fcb08333fa4310
rushi56 <164756999+rushi56@users.noreply.github.com>
2025-11-07 05:16:57 +0530

    Update authMiddleware.js(verify for logged in lister)

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,11 @@
 // middleware/authMiddleware.js
 
+
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.models.js";
+
 // This is our simulated admin check
 const checkAdmin = (req, res, next) => {
   console.log('Checking for admin role...');
@@ -17,4 +23,40 @@ const checkAdmin = (req, res, next) => {
   }
 };
 
-export { checkAdmin };
+
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request");
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const user = await User.findOne({ firebaseUid: decodedToken?.firebaseUid });
+
+        if (!user) {
+            throw new ApiError(401, "Invalid Access Token");
+        }
+
+        req.user = user; 
+        next(); 
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token");
+    }
+});
+
+
+export const verifyLister = asyncHandler(async (req, _, next) => {
+   
+    const userRole = req.user.role;
+
+    if (userRole !== 'lister') {
+        throw new ApiError(403, "Access Denied: You must be a 'lister' to perform this action.");
+    }
+
+    next(); 
+});
+
+export { checkAdmin, verifyJWT, verifyLister };
