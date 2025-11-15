d6b794575655e552cae53ccf332e81564a1dc260
rushi56 <164756999+rushi56@users.noreply.github.com>
2025-11-15 13:22:18 +0530

    Update property.routes.js

--- Backend/src/routes/property.routes.js
+++ Backend/src/routes/property.routes.js
@@ -1,8 +1,48 @@
 import { Router } from "express";
-import { getFilteredProperties } from "../controllers/property.controller.js";
+import { getFilteredProperties
+         createProperty,
+         updatePropertyDetails,
+         deleteProperty,
+         updatePropertyStatus
+       } from "../controllers/property.controller.js";
+
+import { verifyFirebaseToken, verifyLister } from "../middlewares/authMiddleware.js"; 
+
+import { upload } from "../middlewares/multer.middleware.js";
+
 
 const router = Router()
 
 router.route("/").get(getFilteredProperties)
 
+router.route("/create").post(
+    verifyFirebaseToken, 
+    verifyLister, 
+    upload.array("images", 10), 
+    createProperty
+);
+
+router.route("/update-details/:propertyId").patch(
+    verifyFirebaseToken, 
+    verifyLister, 
+    updatePropertyDetails
+);
+
+router.route("/update-status/:propertyId").patch(
+    verifyFirebaseToken, 
+    verifyLister, 
+    updatePropertyStatus
+);
+
+router.route("/delete/:propertyId").delete(
+    verifyFirebaseToken, 
+    verifyLister, 
+    deleteProperty
+);
+
 export default router
+
+
+
+
+
