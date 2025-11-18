b09cef2d4ce20b9635241cf57737374f2e2ca517
Sri Sai Madhava Teja Yanduri <202301138@dau.ac.in>
2025-11-18 15:45:56 +0530

    Revert "Testing" (#5)
    Reverting to previous main branch

--- src/routes/property.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getFilteredProperties } from "../controllers/property.controller.js";
-
-const router = Router()
-
-router.route("/").get(getFilteredProperties)
-
-export default router
